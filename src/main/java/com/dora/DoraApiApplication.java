8ee5c4275e5e70a736eb1ad1b3219821da7fb675
anurag maharshi <anuragmaharshi@gmail.com>
2026-04-21 20:51:12 +0530

    chore(api): Spring Boot 3.5.13 scaffold (pinned for LLD alignment), Java 21, first green build

---
+++ src/main/java/com/dora/DoraApiApplication.java
@@ -0,0 +1,13 @@
+package com.dora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoraApiApplication.class, args);
+	}
+
+}
